fba3e4f233df2979b273c8766aa83001a99925ad
imran <imran@codebehindltd.xyz>
2026-01-18 00:15:28 +0600

    database load

---
+++ ML.NetConsoleDemo/Session_17/Demo.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.ML;
+using ML.NetConsoleDemo.Session_10;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML.NetConsoleDemo.Session_17
+{
+	internal class Demo
+	{
+		public static void Execute()
+		{
+			MLContext context = new();
+
+			var filePath = Path.Combine(AppContext.BaseDirectory, "Session_17", "traning-dataset","*");
+
+			IDataView dataView = context.Data.LoadFromTextFile<InputModel>(path: filePath, hasHeader: false);
+
+			var preview = dataView.Preview();
+		}
+	}
+}
---
+++ ML.NetConsoleDemo/Session_19/Demo.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using ML.NetConsoleDemo.Session_10;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML.NetConsoleDemo.Session_19
+{
+	internal class Demo
+	{
+		public static void Execute() 
+		{
+			MLContext context = new();
+
+			var database = context.Data.CreateDatabaseLoader<InputModel>();
+
+			var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MLNetDemoDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+			string commandText = "SELECT YearsOfExperience, Salary FROM dbo.SalaryData";
+
+			var dataSourse = new DatabaseSource(SqlClientFactory.Instance, connectionString, commandText);
+
+			IDataView dataView = database.Load(dataSourse);
+
+			var preview = dataView.Preview();
+
+		}
+	}
+}
